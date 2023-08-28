2cd3c5e388c3d4aa0626726328197b81d4f085e7
Juan Batty <jmbl1685@hotmail.com>
2023-08-28 11:00:20 -0500

    github action added + server

---
+++ scripts/pull_spinup-live.sh
@@ -0,0 +1,17 @@
+
+echo "Copying Dockerfile"
+echo "Getting current id"
+CURRENT_ID=$(sudo docker ps -a -q --filter ancestor=dii-event-listener-app)
+echo "the current id is $CURRENT_ID"
+echo "Building the DII Site image"
+sudo docker build . -t dii-event-listener-app --file Dockerfile
+echo "Stopping and removing the previous image"
+sudo docker stop $CURRENT_ID
+sudo docker rm $CURRENT_ID
+echo "Running the new image"
+sleep 1s
+sudo docker run -d -p 3000:3000 dii-event-listener-app
+echo "Listing the running containers"
+sudo docker ps
+echo "Cleaning up (prune)"
+sudo docker system prune --force
--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,30 @@
+import 'dotenv/config';
+
 import {
     D2EventListener
 } from "./event-listener";
+
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+
+const PORT = process.env.PORT || 3006;
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'D2 Event Listener'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
 
 (async function main() {
     try {
