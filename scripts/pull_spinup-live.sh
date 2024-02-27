bf5662f8a409920f240c5a2bbd774aafc527b555
Juan Batty <jmbl1685@hotmail.com>
2024-02-27 09:49:08 -0500

    deploy settings updated

--- scripts/pull_spinup-live.sh
+++ scripts/pull_spinup-live.sh
@@ -1,17 +1,34 @@
-
-echo "Copying Dockerfile"
-echo "Getting current id"
-CURRENT_ID=$(sudo docker ps -a -q --filter ancestor=d2-event-listener-app)
-echo "the current id is $CURRENT_ID"
-echo "Building the DII Site image"
-sudo docker build . --no-cache -t d2-event-listener-app --file Dockerfile
-echo "Stopping and removing the previous image"
-sudo docker stop $CURRENT_ID
-sudo docker rm $CURRENT_ID
-echo "Running the new image"
-sleep 1s
-sudo docker run -d -p 3006:3006 d2-event-listener-app
-echo "Listing the running containers"
+echo "Get docker processes"
 sudo docker ps
-echo "Cleaning up (prune)"
-sudo docker system prune --force
+
+echo "Getting current id"
+CURRENT_ID=$(sudo docker images 'd2-event-listener' -a -q)
+echo "the current id is $CURRENT_ID"
+
+echo "Building D2 Event Listener Image"
+
+echo "Building..."
+docker-compose -f docker-compose.yml up --build -no-cache -d --no-start
+
+echo "Deleting/Stopping Demo containers"
+sudo docker rmi $CURRENT_ID
+sleep 1s
+sudo docker stop $CURRENT_ID
+sleep 1s
+sudo docker rm $CURRENT_ID
+sleep 1s
+
+for id in $(docker ps -q)
+do
+    if [[ $(docker port "${id}") == *"3006"* ]]; then
+        echo "stopping container ${id}"
+        docker stop "${id}"
+    fi
+done
+
+echo "Waiting 5 seconds to build again"
+sleep 5s
+docker-compose -f docker-compose.yml up --build -d
+echo "Finished!"
+echo "Get docker processes after the previous changes/updates"
+sudo docker ps
