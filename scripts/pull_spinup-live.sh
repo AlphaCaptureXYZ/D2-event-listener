22818a0ad1de7243abf1693d56d64baa7c42433d
Juan Batty <jmbl1685@hotmail.com>
2024-02-27 10:15:48 -0500

    pull_spinup-live.sh updated

--- scripts/pull_spinup-live.sh
+++ scripts/pull_spinup-live.sh
@@ -37,13 +37,7 @@ echo "Getting the new container id"
 CURRENT_ID=$(sudo docker ps -aqf "name=d2-event-listener")
 echo "the current container id is $CURRENT_ID"
 
-# reset (stop) and start again
-echo "Stopping the current container"
-sudo docker stop $CURRENT_ID
-sleep 5s
-echo "Starting the new container"
-sudo docker start $CURRENT_ID
-sleep 5s
+sleep 60s
 
 # reset (stop) and start again
 echo "Stopping the current container"
@@ -52,7 +46,6 @@ sleep 10s
 echo "Starting the new container"
 sudo docker start $CURRENT_ID
 sleep 5s
-
 
 echo "Get docker processes after the previous stop and start"
 sudo docker ps
