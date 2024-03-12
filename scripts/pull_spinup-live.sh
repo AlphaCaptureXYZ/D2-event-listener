1d5c3906c1dcf735cd9b4a06930ff180a6e4c98c
Juan Batty <jmbl1685@hotmail.com>
2024-03-12 14:06:47 -0500

    pull_spinup-live.sh updated

--- scripts/pull_spinup-live.sh
+++ scripts/pull_spinup-live.sh
@@ -22,25 +22,20 @@ docker-compose -f docker-compose.yml up --build --no-start
 sleep 5s
 docker-compose up --build -d
 
+sleep 120s
+
+echo "Awaiting to reset"
+
+echo "Stopping the current container"
+sudo docker stop $CURRENT_ID
+sleep 120s
+echo "Starting the new container"
+sudo docker start $CURRENT_ID
+sleep 5s
+
 echo "Finished!"
 echo "Get docker processes after the previous changes/updates"
 sudo docker ps
-
-# sleep 10s
-
-# echo "Getting the new container id"
-# CURRENT_ID=$(sudo docker ps -aqf "name=d2-event-listener")
-# echo "the current container id is $CURRENT_ID"
-
-# sleep 60s
-
-# # reset (stop) and start again
-# echo "Stopping the current container"
-# sudo docker stop $CURRENT_ID
-# sleep 10s
-# echo "Starting the new container"
-# sudo docker start $CURRENT_ID
-# sleep 5s
 
 # echo "Get docker processes after the previous stop and start"
 # sudo docker ps
