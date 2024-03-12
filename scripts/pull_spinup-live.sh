8ee0c5e0b865425243c21c4d66c2ea647a24cd0b
Juan Batty <jmbl1685@hotmail.com>
2024-03-12 13:26:01 -0500

    reset.sh added

--- scripts/pull_spinup-live.sh
+++ scripts/pull_spinup-live.sh
@@ -33,19 +33,5 @@ echo "Finished!"
 echo "Get docker processes after the previous changes/updates"
 sudo docker ps
 
-echo "Getting the new container id"
-CURRENT_ID=$(sudo docker ps -aqf "name=d2-event-listener")
-echo "the current container id is $CURRENT_ID"
+sleep 60s
 
-sleep 120s
-
-# reset (stop) and start again
-echo "Stopping the current container"
-sudo docker stop $CURRENT_ID
-sleep 120s
-echo "Starting the new container"
-sudo docker start $CURRENT_ID
-sleep 20s
-
-echo "Get docker processes after the previous stop and start"
-sudo docker ps
