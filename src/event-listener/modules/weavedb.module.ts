26df2135e71a74bad2bd4b5afa464ac081583ebb
ssribeiro <luan@ssribeiro.com>
2024-05-07 17:17:08 -0300

    added retry logic

--- src/event-listener/modules/weavedb.module.ts
+++ src/event-listener/modules/weavedb.module.ts
@@ -1,345 +1,330 @@
 import * as config from "../config/config";
 
-import { LitModule } from './lit.module';
-import { CompressorModule } from './compressor.module';
-import { NftStorageModule } from './nft-store.module';
+import { LitModule } from "./lit.module";
+import { CompressorModule } from "./compressor.module";
+import { NftStorageModule } from "./nft-store.module";
 
 import { ethers } from "ethers";
 
-import * as WeaveDB from 'weavedb-sdk-node';
+import * as WeaveDB from "weavedb-sdk-node";
 
-import { getStringSize, isNullOrUndefined } from '../helpers/helpers';
+import { getStringSize, isNullOrUndefined } from "../helpers/helpers";
 
-import { blobToBase64String } from '@lit-protocol/lit-node-client-nodejs';
+import { blobToBase64String } from "@lit-protocol/lit-node-client-nodejs";
 import { getCurrentWalletAddress } from "../utils/utils";
 
-const COLLECTION_NAME = 'D2-data';
+const COLLECTION_NAME = "D2-data";
 
 let db: any = null;
 
 const init = async () => {
-    if (isNullOrUndefined(db)) {
+  if (isNullOrUndefined(db)) {
+    // console.log('weave db A', db);
 
-        // console.log('weave db A', db);
+    try {
+      const contractTxId = config.WEAVEDB_CONTRACT_TX_ID;
+      // console.log('contractTxId', contractTxId);
 
-        try {
-            const contractTxId = config.WEAVEDB_CONTRACT_TX_ID;
-            // console.log('contractTxId', contractTxId);
+      db = new WeaveDB({
+        contractTxId,
+        nocache: true,
+      });
+      // console.log('weave db', db);
 
-            db = new WeaveDB({
-                contractTxId,
-                nocache: true,
-            });
-            // console.log('weave db', db);
+      await db.initializeWithoutWallet();
 
-            await db.initializeWithoutWallet();
+      const privateKey = config.WALLET_PRIVATE_KEY;
+      // console.log('privateKey', privateKey);
 
-            const privateKey = config.WALLET_PRIVATE_KEY;
-            // console.log('privateKey', privateKey);
+      if (privateKey) {
+        const address = getCurrentWalletAddress();
 
-            if (privateKey) {
-                const address = getCurrentWalletAddress();
+        const config = {
+          getAddressString: () => address?.toLowerCase(),
+          getPrivateKey: () => Buffer.from(privateKey, "hex"),
+        };
 
-                const config = {
-                    getAddressString: () => address?.toLowerCase(),
-                    getPrivateKey: () => Buffer.from(privateKey, 'hex'),
-                };
-
-                await db.setDefaultWallet(config, 'evm');
-            }
-
-        } catch (err: any) {
-            console.log('[weavedb] init (error)', err?.message);
-        }
+        await db.setDefaultWallet(config, "evm");
+      }
+    } catch (err: any) {
+      console.log("[weavedb] init (error)", err?.message);
     }
-    return db;
-}
+  }
+  return db;
+};
 
 const accessControlConditions = (
-    chain: string,
-    userWallet: string,
-    pkpWalletAddress?: string,
+  chain: string,
+  userWallet: string,
+  pkpWalletAddress?: string
 ) => {
-    // https://developer.litprotocol.com/accessControl/EVM/basicExamples#a-specific-wallet-address
-    const accessControlConditions: any[] = [
-        {
-            contractAddress: '',
-            standardContractType: '',
-            chain,
-            method: '',
-            parameters: [
-                ':userAddress',
-            ],
-            returnValueTest: {
-                comparator: '=',
-                value: userWallet?.toLowerCase(),
-            },
-        },
-    ];
+  // https://developer.litprotocol.com/accessControl/EVM/basicExamples#a-specific-wallet-address
+  const accessControlConditions: any[] = [
+    {
+      contractAddress: "",
+      standardContractType: "",
+      chain,
+      method: "",
+      parameters: [":userAddress"],
+      returnValueTest: {
+        comparator: "=",
+        value: userWallet?.toLowerCase(),
+      },
+    },
+  ];
 
-    if (pkpWalletAddress) {
-        accessControlConditions.push({
-            operator: "or",
-        });
-        accessControlConditions.push({
-            contractAddress: '',
-            standardContractType: '',
-            chain,
-            method: '',
-            parameters: [
-                ':userAddress',
-            ],
-            returnValueTest: {
-                comparator: '=',
-                value: pkpWalletAddress,
-            },
-        });
-    }
+  if (pkpWalletAddress) {
+    accessControlConditions.push({
+      operator: "or",
+    });
+    accessControlConditions.push({
+      contractAddress: "",
+      standardContractType: "",
+      chain,
+      method: "",
+      parameters: [":userAddress"],
+      returnValueTest: {
+        comparator: "=",
+        value: pkpWalletAddress,
+      },
+    });
+  }
 
-    return accessControlConditions;
-}
+  return accessControlConditions;
+};
 
 const getAllData = async <T>(
-    chain: string,
-    payload: {
-        type: string,
-        byUserWalletFilter?: boolean,
-        wallet?: string,
-    },
-    authSig: any = null,
+  chain: string,
+  payload: {
+    type: string;
+    byUserWalletFilter?: boolean;
+    wallet?: string;
+  },
+  authSig: any = null
 ) => {
-    let data = [];
+  let data = [];
 
-    try {
-        const {
-            byUserWalletFilter,
-            type,
-            wallet,
-        } = payload;
+  try {
+    const { byUserWalletFilter, type, wallet } = payload;
 
-        await init();
+    await init();
 
-        let docs = [] as any[];
+    let docs = [] as any[];
 
-        try {
-            console.log('[weavedb] loading info...');
-            docs = await db.cget(
-                COLLECTION_NAME,
-                ['type'],
-                ['type', '==', type],
-            );
-            console.log('[weavedb] info loaded!');
-        } catch (err: any) {
-            console.log('[weavedb] getAllData (error "db.cget(...)")', err?.message);
-        }
-
-        if (byUserWalletFilter) {
-            const userAddress = wallet ? wallet?.toLowerCase() : getCurrentWalletAddress();
-
-            docs = docs?.filter((doc) => {
-                return doc?.data?.userAddress === userAddress;
-            });
-        }
-
-        data = (await Promise?.all(docs?.map(async (res) => {
-            try {
-                const docId = res?.id;
-                const info: any = res?.data;
-
-                if ([
-                    'order',
-                ].includes(type)) {
-                    const cid = info?.data;
-
-                    const nftStorageResult = await NftStorageModule.retrieve({
-                        cid,
-                    });
-
-                    info.data = nftStorageResult;
-                }
-
-                const userWallet = info?.userAddress;
-                const pkpWalletAddress = info?.pkpWalletAddress || null;
-
-                const dataIsCompressed = info?.isCompressed || false;
-
-                const acConditions = accessControlConditions(chain, userWallet, pkpWalletAddress);
-
-                let doc = null as any;
-
-                if (dataIsCompressed) {
-                    doc = await CompressorModule.decompressData(info?.data);
-                    info.data = doc;
-                };
-
-                doc = JSON.parse(atob(info?.data));
-
-                const {
-                    encryptedData,
-                    encryptedSymmetricKey,
-                } = doc;
-
-                let decryptedFile = null;
-
-                if (isNullOrUndefined(authSig)) {
-                    decryptedFile = await LitModule().decryptString(
-                        encryptedData,
-                        encryptedSymmetricKey,
-                        acConditions,
-                        chain,
-                    );
-                }
-
-                if (!isNullOrUndefined(authSig)) {
-                    decryptedFile = await LitModule().decryptStringByPkp(
-                        authSig,
-                        chain,
-                        encryptedData,
-                        encryptedSymmetricKey,
-                        acConditions,
-                    );
-                }
-
-                const decryptedString = JSON.parse(decryptedFile);
-
-                if (decryptedString) {
-                    decryptedString.docId = docId;
-                    decryptedString.pkpWalletAddress = pkpWalletAddress;
-                }
-
-                return decryptedString;
-
-            } catch (err: any) {
-                // console.log('[weavedb] getAllData (error)', err?.message);
-            }
-        }))) || [];
-
-        data = data?.filter((item) => item) || [];
-
-    } catch (e) {
-        console.error(e);
+    let done: boolean = false;
+    while (done === false) {
+      await new Promise((r) => setTimeout(r, 1000));
+      try {
+        console.log("[weavedb] loading info...");
+        docs = await db.cget(COLLECTION_NAME, ["type"], ["type", "==", type]);
+        console.log("[weavedb] info loaded!");
+        done = true;
+      } catch (err: any) {
+        console.log(
+          '[weavedb] getAllData (error "db.cget(...)")',
+          err?.message
+        );
+        console.log("retrying..");
+        await new Promise((r) => setTimeout(r, 5000));
+      }
     }
 
-    return data as T[];
-}
+    if (byUserWalletFilter) {
+      const userAddress = wallet
+        ? wallet?.toLowerCase()
+        : getCurrentWalletAddress();
+
+      docs = docs?.filter((doc) => {
+        return doc?.data?.userAddress === userAddress;
+      });
+    }
+
+    data =
+      (await Promise?.all(
+        docs?.map(async (res) => {
+          try {
+            const docId = res?.id;
+            const info: any = res?.data;
+
+            if (["order"].includes(type)) {
+              const cid = info?.data;
+
+              const nftStorageResult = await NftStorageModule.retrieve({
+                cid,
+              });
+
+              info.data = nftStorageResult;
+            }
+
+            const userWallet = info?.userAddress;
+            const pkpWalletAddress = info?.pkpWalletAddress || null;
+
+            const dataIsCompressed = info?.isCompressed || false;
+
+            const acConditions = accessControlConditions(
+              chain,
+              userWallet,
+              pkpWalletAddress
+            );
+
+            let doc = null as any;
+
+            if (dataIsCompressed) {
+              doc = await CompressorModule.decompressData(info?.data);
+              info.data = doc;
+            }
+
+            doc = JSON.parse(atob(info?.data));
+
+            const { encryptedData, encryptedSymmetricKey } = doc;
+
+            let decryptedFile = null;
+
+            if (isNullOrUndefined(authSig)) {
+              decryptedFile = await LitModule().decryptString(
+                encryptedData,
+                encryptedSymmetricKey,
+                acConditions,
+                chain
+              );
+            }
+
+            if (!isNullOrUndefined(authSig)) {
+              decryptedFile = await LitModule().decryptStringByPkp(
+                authSig,
+                chain,
+                encryptedData,
+                encryptedSymmetricKey,
+                acConditions
+              );
+            }
+
+            const decryptedString = JSON.parse(decryptedFile);
+
+            if (decryptedString) {
+              decryptedString.docId = docId;
+              decryptedString.pkpWalletAddress = pkpWalletAddress;
+            }
+
+            return decryptedString;
+          } catch (err: any) {
+            // console.log('[weavedb] getAllData (error)', err?.message);
+          }
+        })
+      )) || [];
+
+    data = data?.filter((item) => item) || [];
+  } catch (e) {
+    console.error(e);
+  }
+
+  return data as T[];
+};
 
 const addData = async <T>(
-    chain: string,
-    payload: {
-        // data to store based on type
-        jsonData: T,
-        // user wallet address
-        userWallet: string,
-        // type of data to store (setting, trigger, profile, order, etc)
-        type: string,
-        // pkp key to store data for enable external access
-        pkpKey: string,
-        // if data is compressed
-        isCompressed?: boolean,
-        // to store data via IPFS (data with big size)
-        // viaIPFS?: boolean,
-        additionalInfo?: any,
-    },
-    authSig: any = null,
+  chain: string,
+  payload: {
+    // data to store based on type
+    jsonData: T;
+    // user wallet address
+    userWallet: string;
+    // type of data to store (setting, trigger, profile, order, etc)
+    type: string;
+    // pkp key to store data for enable external access
+    pkpKey: string;
+    // if data is compressed
+    isCompressed?: boolean;
+    // to store data via IPFS (data with big size)
+    // viaIPFS?: boolean,
+    additionalInfo?: any;
+  },
+  authSig: any = null
 ) => {
+  let objEstimatedSize: any = null;
+  let result: any = null;
 
-    let objEstimatedSize: any = null;
-    let result: any = null;
+  try {
+    await init();
 
-    try {
-        await init();
+    let { jsonData, userWallet, type, pkpKey, isCompressed, additionalInfo } =
+      payload;
 
-        let {
-            jsonData,
-            userWallet,
-            type,
-            pkpKey,
-            isCompressed,
-            additionalInfo,
-        } = payload;
+    userWallet = userWallet?.toLowerCase();
 
-        userWallet = userWallet?.toLowerCase();
+    const pkpWalletAddress = ethers.utils.computeAddress(pkpKey);
 
-        const pkpWalletAddress = ethers.utils.computeAddress(pkpKey);
+    const acConditions = accessControlConditions(
+      chain,
+      userWallet,
+      pkpWalletAddress
+    );
 
-        const acConditions = accessControlConditions(chain, userWallet, pkpWalletAddress);
+    const { encryptedFile, encryptedSymmetricKeyString } =
+      await LitModule().encryptString(
+        JSON.stringify(jsonData),
+        acConditions,
+        true,
+        chain,
+        authSig
+      );
 
-        const {
-            encryptedFile,
-            encryptedSymmetricKeyString,
-        } = await LitModule().encryptString(
-            JSON.stringify(jsonData),
-            acConditions,
-            true,
-            chain,
-            authSig,
-        );
+    const encryptedFileB64 = await blobToBase64String(encryptedFile);
 
-        const encryptedFileB64 = await blobToBase64String(encryptedFile);
+    const id = Math.random().toString(36).substring(7);
 
-        const id = Math.random().toString(36).substring(7);
+    const info = {
+      encryptedData: encryptedFileB64,
+      encryptedSymmetricKey: encryptedSymmetricKeyString,
+    };
 
-        const info = {
-            encryptedData: encryptedFileB64,
-            encryptedSymmetricKey: encryptedSymmetricKeyString,
-        };
+    let data = Buffer.from(JSON.stringify(info)).toString("base64");
 
-        let data = Buffer.from(JSON.stringify(info)).toString('base64');
-
-        if (payload?.isCompressed) {
-            data = await CompressorModule.compressData(data);
-        }
-
-        if ([
-            'order',
-        ].includes(type)) {
-            const { cid } = await NftStorageModule.add(data);
-            data = cid;
-        }
-
-        const obj = {
-            id,
-            createdAt: Date.now(),
-            data,
-            type,
-            userAddress: userWallet,
-            pkpWalletAddress,
-            isCompressed: isCompressed || false,
-            additionalInfo,
-        };
-
-        if (isNullOrUndefined(pkpWalletAddress)) {
-            delete obj.pkpWalletAddress;
-        }
-
-        objEstimatedSize = getStringSize(JSON.stringify(obj));
-
-        const tx = await db.add(
-            obj,
-            COLLECTION_NAME,
-        );
-
-        result = tx;
-
-    } catch (err: any) {
-        // todo: handle error
-        console.log('[weavedb] addData (error)', err?.message);
-        console.log('[weavedb] addData (estimated size)', objEstimatedSize);
+    if (payload?.isCompressed) {
+      data = await CompressorModule.compressData(data);
     }
 
-    return result;
-}
+    if (["order"].includes(type)) {
+      const { cid } = await NftStorageModule.add(data);
+      data = cid;
+    }
 
-const deleteData = async (
-    docId: string,
-) => {
-    await init();
-    const result = await db.delete(COLLECTION_NAME, docId);
-    return result;
+    const obj = {
+      id,
+      createdAt: Date.now(),
+      data,
+      type,
+      userAddress: userWallet,
+      pkpWalletAddress,
+      isCompressed: isCompressed || false,
+      additionalInfo,
+    };
+
+    if (isNullOrUndefined(pkpWalletAddress)) {
+      delete obj.pkpWalletAddress;
+    }
+
+    objEstimatedSize = getStringSize(JSON.stringify(obj));
+
+    const tx = await db.add(obj, COLLECTION_NAME);
+
+    result = tx;
+  } catch (err: any) {
+    // todo: handle error
+    console.log("[weavedb] addData (error)", err?.message);
+    console.log("[weavedb] addData (estimated size)", objEstimatedSize);
+  }
+
+  return result;
+};
+
+const deleteData = async (docId: string) => {
+  await init();
+  const result = await db.delete(COLLECTION_NAME, docId);
+  return result;
 };
 
 export const WeaveDBModule = {
-    addData,
-    getAllData,
-    deleteData,
+  addData,
+  getAllData,
+  deleteData,
 };
